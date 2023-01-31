cb3430f9a89ba0c5ceaab2cc21035905e0943484
zmeditation <senior.zsoftware@gmail.com>
2023-01-31 21:14:44 +0000

    Update resources files

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -3,7 +3,7 @@
     <!-- #### Area Content ###-->
     @section('content')
         @foreach($tabSection AS $sect)
-            @php($var = 'data' . ucfirst(camelCase($sect['view'])))
+            @php($var = 'data' . ucfirst( $sect['view']) )
             @php($$var = $data[$sect['view']] ?? [])
             @php($donnees = $sect)
             @include("sections.$sect[view]", compact($var, 'donnees', 'page'))
---
+++ resources/views/sections/about.blade.php
@@ -0,0 +1,1 @@
+<h1>Bienvenu sur Woody Builder</h1>
